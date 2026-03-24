ae752de8d4cb9a87bb7740f95211c3e9ced8796d
Ezequiel C. <e.cf@leiuq.fun>
2026-03-24 22:21:44 +0100

    better code than what is created with auth command.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,12 +4,14 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./auth-schema";
 
+// TODO: Move this database init to another file
 const db = drizzle(new Pool({ connectionString: process.env.DATABASE_URL }), {
   schema,
 });
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", schema }),
+  appName: "e-Sign",
   baseURL: "http://localhost:3000/",
   emailAndPassword: { enabled: true },
   socialProviders: {
@@ -21,5 +23,20 @@ export const auth = betterAuth({
       clientId: process.env.MICROSOFT_CLIENT_ID!,
       clientSecret: process.env.MICROSOFT_CLIENT_SECRET!,
     },
+  },
+  session: {
+    freshAge: 60,
+  },
+  rateLimit: {
+    enabled: true,
+    max: 50,
+  },
+  advanced: {
+    cookiePrefix: "esauth",
+  },
+  plugins: [],
+  telemetry: {
+    enabled: false,
+    debug: false,
   },
 });
