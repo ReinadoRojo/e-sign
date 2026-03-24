a1a3ef4855f91404cfd31e43657ac0c32eafd81e
Ezequiel C. <e.cf@leiuq.fun>
2026-03-24 21:39:40 +0100

    changed to better auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "@propelauth/nextjs/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><AuthProvider authUrl={process.env.NEXT_PUBLIC_AUTH_URL!}>{children}</AuthProvider></body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
